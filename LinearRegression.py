ffd077bb6266c818c9cd70876a2db8aa50a8a3fe
michaelcote14 <michaelcote14@gmail.com>
2022-09-04 12:44:14 -0500

    first commit

--- LinearRegression.py
+++ LinearRegression.py
@@ -7,5 +7,10 @@ X = df[['G1', 'G2', 'absences', 'age']] #puts all this data for these columns in
 y = df[['G3']] #puts your dependant variable into y
 
 from sklearn import linear_model
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
 
+predictedG3 = regr.predict([[11, 12, 2, 20]]) #this will predict a G3 based on the inputed G1, G2, absences, and age
+print(predictedG3)
 
+print(regr.coef_) #this tells the coefficients for each input, aka the impact they have
