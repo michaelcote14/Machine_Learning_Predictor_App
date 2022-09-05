2ce8fbf4fc6350e5f03d14912d1cb13af3ab7093
michaelcote14 <michaelcote14@gmail.com>
2022-09-04 22:46:56 -0500

    first commit

--- LinearRegression.py
+++ LinearRegression.py
@@ -8,9 +8,17 @@ y = df[['G3']] #puts your dependant variable into y
 
 from sklearn import linear_model
 regr = linear_model.LinearRegression()
-regr.fit(X, y)
+regr.fit(X, y) #creates the regression line
 
-predictedG3 = regr.predict([[11, 12, 2, 20]]) #this will predict a G3 based on the inputed G1, G2, absences, and age
-print(predictedG3)
+predictedG3 = regr.predict([[5, 6, 3, 20]]) #this will predict a G3 based on the inputed G1, G2, absences, and age
+print('predictedG3:', predictedG3)
 
+#this section scales the data to be more equal
+from sklearn.preprocessing import StandardScaler
+scale = StandardScaler()
+scaledX = scale.fit_transform(X)
+print(scaledX)
+
+
+print('new') #just so I can see where the new line is
 print(regr.coef_) #this tells the coefficients for each input, aka the impact they have
