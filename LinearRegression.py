40e10c476fda023ff540a6856d57bbda046e7a2a
Michael Cote <michaelcote14@gmail.com>
2022-09-10 19:47:49 -0500

    9/5/22

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,42 +3,58 @@ import sklearn
 from sklearn import linear_model
 import numpy as np
 
-#option 1
-#df = pd.read_csv("student-mat.csv", sep=";")
-#option 2
 Dataframe = pd.read_csv("student-mat.csv", sep=';')
 DataPicks = Dataframe[["G1", "G2", "G3", "studytime", "failures", "absences"]]
-PredictorVariableG3 = "G3"
+TargetVariable = "G3"
+print(DataPicks.describe())
 
-#option 1
-# X = df[['G1', 'G2', 'studytime', 'failures', 'absences']] #puts all this data for these columns in X for your independant variables
-# y = df[['G3']] #puts your dependant variable into y
-#option 2
-X = np.array(DataPicks.drop([PredictorVariableG3], axis=1))
-y = np.array(DataPicks[PredictorVariableG3])
+X = np.array(DataPicks.drop([TargetVariable], axis=1))
+y = np.array(DataPicks[TargetVariable])
+print("Below Data Are All Correlations")
+print(DataPicks.corr())
 
 
 ##this puts the data into 4 different arrays: x train, x test, y train, and y test, the random_state parameter chooses how to randomly split the data. not specifying changes it each time.
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1) #add in randomstate= a # to stop randomly changing your arrays
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1, random_state=0) #add in randomstate= a # to stop randomly changing your arrays
 
 
 MyLinearRegression = linear_model.LinearRegression()
 MyLinearRegression.fit(x_train, y_train) #creates the regression line
 
+#see if you can get coefficients to be prefaced by what they represent
+print('Coefficients:', MyLinearRegression.coef_) #this tells the coefficients for each input, aka the impact they have
+print('y-intercept:', MyLinearRegression.intercept_)
 
-predictedG3 = MyLinearRegression.predict([[6, 9, 1, 2, 14]]) #this will predict a G3 based on the inputed G1, G2, studytime, failures, and absences
-print('predictedG3:', predictedG3)
+TargetVariablePrediction = MyLinearRegression.predict([[10, 13, 1, 0, 6]]) #this will predict a G3 based on the inputed G1, G2, studytime, failures, and absences
+CurrentModelAccuracy = MyLinearRegression.score(x_test, y_test)
+print('Current Model Prediction:', TargetVariablePrediction, 'Accuracy:', CurrentModelAccuracy, "Range:", TargetVariablePrediction-CurrentModelAccuracy*0.01*TargetVariablePrediction, "-", CurrentModelAccuracy*0.01*TargetVariablePrediction+TargetVariablePrediction)
 
 import pickle
 pickle_in = open("studentmodel.pickle", "rb") #loads the prediction model into the variable "linear"
-picklemodelG3 = pickle.load(pickle_in)
-pickleprediction = picklemodelG3.predict([[6, 9, 1, 2, 14]])
-print('pickleprediction:', pickleprediction)
-
+PickleTargetVariablePrediction = pickle.load(pickle_in)
+try:
+    PicklePrediction = PickleTargetVariablePrediction.predict([[10, 13, 1, 0, 6]])
+    PickleModelAccuracy = PickleTargetVariablePrediction.score(x_test, y_test)
+    print('Pickle Model Prediction:', PicklePrediction, 'Accuracy:', PickleModelAccuracy, "Range:", PicklePrediction-PickleModelAccuracy*0.01*PicklePrediction, "-", PicklePrediction+PickleModelAccuracy*0.01*PicklePrediction)
+except:
+    pass
+Sum = 0
 MyModelsPredictions = MyLinearRegression.predict(x_test)
-for x in range(len(MyModelsPredictions)):
-    print(MyModelsPredictions[x], x_test[x], y_test[x])
-
+print("Do you want to run evaluations on all data? Y=Yes N=No")
+IndividualDataDecision = input()
+if (IndividualDataDecision == 'Y'):
+    print("Predicted         [Actual Data]  Actual Score       Difference")
+    for x in range(len(MyModelsPredictions)):
+        print(MyModelsPredictions[x], ",",  x_test[x], ",",  y_test[x], "," "            Difference:", y_test[x]-MyModelsPredictions[x])
+        IndividualDifference = abs(y_test[x] - MyModelsPredictions[x])
+        Sum = Sum+IndividualDifference
+        Max = 0
+        if IndividualDifference > Max:
+            Max = IndividualDifference
+print('Sum Difference:', Sum)
+print('Average Difference:', Sum/len(MyModelsPredictions))
+print('Max Difference:', Max) #maybe use an absolute value?
+print(len(MyModelsPredictions))
 # #this section scales the data to be more equal
 # from sklearn.preprocessing import StandardScaler
 # scale = StandardScaler()
@@ -46,11 +62,5 @@ for x in range(len(MyModelsPredictions)):
 # print(scaledX)
 
 
-print('Coefficients:', MyLinearRegression.coef_) #this tells the coefficients for each input, aka the impact they have
 
-#shows how accurate your current regression model is
-CurrentModelAccuracy = MyLinearRegression.score(x_test, y_test)
-print('Current Model Accuracy:', CurrentModelAccuracy)
-PickleModelAccuracy = picklemodelG3.score(x_test, y_test)
-print("Pickle Model Accuracy:", PickleModelAccuracy)
 
