27309b28b0cc66247970c6d82ac87392af5a901c
Michael Cote <michaelcote14@gmail.com>
2022-09-19 14:31:42 -0500

    added visualization file and started on the feature iterator2 file

--- FeatureIteratorTester2.py
+++ FeatureIteratorTester2.py
@@ -2,6 +2,7 @@ import pandas as pd #this is to read in data sheets
 import numpy as np #this is for doing interesting things wtih numbers
 import sklearn #this is the machine learning module
 from sklearn import linear_model
+import itertools
 from sklearn.utils import shuffle
 import matplotlib.pyplot as pyplot #this allows you to make graphs
 import pickle #this saves your model for the machine and keeps you from having to retrain plus it saves your most accurate model
@@ -12,34 +13,24 @@ Dataframe = pd.read_csv("Feature Optimizer Data.csv", sep=',')
 print("Dataframe Columns:", Dataframe.columns)
 DataColumns = Dataframe.columns
 DataFeatures = DataColumns.drop("G3")
+DataFeaturesList = DataFeatures.tolist()
+print(DataFeaturesList)
 print("DataFeatures:", DataFeatures)
 
 
 
-#what we need: Dataframe[["Medu", "Fedu", "G1", "G2", "studytime", "famrel", "freetime", "traveltime", "failures", "health", "Walc", "Dalc", "G3"]]
-TargetVariable = "G3"
-BestScore = 0
-BestFeatures = []
-for Column1 in DataFeatures:
-    for Column2 in DataFeatures:
-        for Column3 in DataFeatures:
-            Features = [Column1, Column2, Column3]
-            print("Features:", Features)
-            DataPicks = Dataframe[[Column1, Column2, Column3]]
-            X = np.array(DataPicks) #this is not an array yet, but its supposed to be
-            y = np.array(Dataframe[TargetVariable])
 
 
-            x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1, random_state=0)  # add in randomstate= a # to stop randomly changing your arrays
+combinations = 0
+for i in DataFeaturesList:
+    result = itertools.combinations(DataFeaturesList, DataFeaturesList.index(i)+1)
+    print("i:", i)
+    for item in result:
+        print(item)
+        combinations = combinations + 1
+print("combinations:", combinations)
 
-            MyLinearRegression = linear_model.LinearRegression()
-            MyLinearRegression.fit(x_train, y_train)  # creates the regression line
-            MyLinearRegressionScore = MyLinearRegression.score(x_test, y_test)
-            print("Current Model Score:", MyLinearRegressionScore, "\n")
-            if MyLinearRegressionScore > BestScore:
-                BestScore = MyLinearRegressionScore
-                BestFeatures = Features
 
-print("Best Score:", BestScore)
-print("Best Features:", BestFeatures)
+
+
 
---
+++ Visualization.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+def rmse(targets, predictions):
+    return np.sqrt(np.mean(np.square(targets - predictions)))
+UploadFile = "Feature Optimizer Data.csv"
+Dataframe = pd.read_csv(UploadFile, sep=',')
+PickedFeatures = ['Fedu', 'Medu', 'age', 'G3']
+DataPicks = Dataframe[PickedFeatures]
+TargetVariable = "G3"
+X = np.array(DataPicks.drop([TargetVariable], axis=1))
+y = np.array(DataPicks[TargetVariable])
+
+
+x_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1) #add in randomstate= a # to stop randomly changing your arrays
+
+MyLinearRegression = linear_model.LinearRegression().fit(x_train, y_train)
