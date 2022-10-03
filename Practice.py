a1e3e53fc43cdd2fb4d4389751c89db12b39ab7a
Michael Cote <michaelcote14@gmail.com>
2022-10-03 14:44:49 -0500

    made dataframe changes permanent in practice module

--- Practice.py
+++ Practice.py
@@ -6,7 +6,10 @@ pd.options.display.width = 500
 pd.set_option('display.max_columns', 500)
 data = pd.read_csv("data/student-mat.csv")
 
+all_after_drop_dataframes = pd.DataFrame()
 
+appended_data_frame = pd.DataFrame()
+print('empty data frame:\n', appended_data_frame)
 for column in data:
     print('column:\n',column)
     if data[column].dtypes == 'object':
@@ -14,7 +17,7 @@ for column in data:
             print('not an integer\n')
 
             series_to_transform = data[[column]]
-            print('chosen_data:\n', series_to_transform)
+            print('series_to_transform:\n', series_to_transform)
 
             transformer_function = make_column_transformer((OneHotEncoder(), [column]), remainder='passthrough')
             print('transformer:\n', transformer_function)
@@ -27,10 +30,17 @@ for column in data:
             column_to_drop = transformed_dataframe.columns[0]
             print('column to drop:\n', column_to_drop)
 
-            series_after_drop = transformed_dataframe.drop(columns=str(column_to_drop))
-            print('series after drop:\n', series_after_drop)
+            dataframe_after_drop = transformed_dataframe.drop(columns=str(column_to_drop))
+            print('series after drop:\n', dataframe_after_drop)
 
-            #ToDo append the above dataframe with the newly made dataframe each time
+
+            all_after_drop_dataframes = pd.concat([all_after_drop_dataframes, dataframe_after_drop], axis=1)
+
+            originaldf_column_to_drop = data.drop(columns=str(column))
+            print('originaldf_column_to_drop:\n', originaldf_column_to_drop)
+            #ToDo fix the above
+
+            # ToDo drop the original columns that aren't yet onehot encoded
 
 
 
@@ -46,5 +56,13 @@ for column in data:
 
     else:
         print('integer\n')
+
+
+# print('final dataframe:\n', final_data_frame)
+print('all after drop dataframes:\n', all_after_drop_dataframes)
+final_dataframe = pd.concat([data, all_after_drop_dataframes], axis=1)
+print('final dataframe:\n', final_dataframe)
+
+
 
 
